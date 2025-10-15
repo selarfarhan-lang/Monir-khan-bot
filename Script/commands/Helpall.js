3737d8efbb41e2204c578fa5bb5764a524b21952
selarfarhan-lang <selarfarhan@gmail.com>
2025-10-16 00:21:24 +0600

    Update Helpall.js

--- Script/commands/Helpall.js
+++ Script/commands/Helpall.js
@@ -5,7 +5,7 @@ module.exports.config = {
  name: "helpall",
  version: "1.0.0",
  hasPermssion: 0,
- credits: "𝐒𝐡𝐚𝐡𝐚𝐝𝐚𝐭 𝐒𝐀𝐇𝐔",
+ credits: "𝐅𝐚𝐫𝐡𝐚𝐧 𝐕𝐀𝐔",
  description: "Displays all available commands in one page",
  commandCategory: "system",
  usages: "[No args]",
@@ -29,8 +29,8 @@ module.exports.run = async function ({ api, event }) {
  const finalText = `╔═══❖ 🌟 𝐂𝐎𝐌𝐌𝐀𝐍𝐃 𝐋𝐈𝐒𝐓 🌟 ❖═══╗
 ${allCommands.map(cmd => `║ ➔ ${cmd}`).join("\n")}
 ╠═════🔰 𝐁𝐎𝐓 𝐈𝐍𝐅𝐎 🔰═════╣
-║ 🤖 𝐁𝐨𝐭: ─꯭─⃝‌‌𝐒𝐡𝐚𝐡𝐚𝐝𝐚𝐭 𝐂𝐡𝐚𝐭 𝐁𝐨𝐭
-║ 👑 𝐎𝐰𝐧𝐞𝐫: 𝐒𝐇𝐀𝐇𝐀𝐃𝐀𝐓 𝐒𝐀𝐇𝐔
+║ 🤖 𝐁𝐨𝐭: ─꯭─⃝‌‌𝐅𝐚𝐫𝐡𝐚𝐧 𝐂𝐡𝐚𝐭 𝐁𝐨𝐭
+║ 👑 𝐎𝐰𝐧𝐞𝐫: 𝐅𝐀𝐑𝐇𝐀𝐍 𝐕𝐀𝐔
 ║ 📦 𝐂𝐨𝐦𝐦𝐚𝐧𝐝𝐬: ${allCommands.length} 
 ╚═══════════════════════╝`;
 
