9a6e63599f2fe06034f095f030dadfb455ff40b6
selarfarhan-lang <selarfarhan@gmail.com>
2025-10-16 00:56:36 +0600

    Update adbot.js

--- Script/commands/adbot.js
+++ Script/commands/adbot.js
@@ -2,7 +2,7 @@ module.exports.config = {
     name: "ckbot",
     version: "1.0.0",
     hasPermssion: 0,
-    credits: "𝐒𝐇𝐀𝐇𝐀𝐃𝐀𝐓 𝐒𝐀𝐇𝐔",
+    credits: "𝐅𝐀𝐑𝐇𝐀𝐍 𝐕𝐀𝐔",
     description: "DESCRIPTION ABOUT BOT",
     commandCategory: "Media",
     usages: "",
